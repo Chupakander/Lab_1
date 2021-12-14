c20135c9a35f7c8158e9e06af492ee958f5ca1d5
Chupa <matvey.savchenko.0202@mail.ru>
2021-12-14 22:58:33 +0300

    update Sandwich

--- src/company/Sandwich.java
+++ src/company/Sandwich.java
@@ -1,4 +1,16 @@
 package company;
 
-public class Sandwich {
+public class Sandwich extends Food {
+
+    public Sandwich(){
+        super ("Sandwich");
+    }
+
+    public void consume(){
+        System.out.println(this + " съеден");
+    }
+    @Override
+    public int calculateCalories() {
+        return 400;
+    }
 }
